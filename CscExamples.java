f4ee2131170e1848afdff7da74af23f4b6413900
[Dilki-Hasara] <[dhwways@gmail.com]>
2021-04-01 08:25:40 -0700

    first commit

---
+++ CscExamples.java
@@ -0,0 +1,34 @@
+public class CscExample{
+	private int questionNo;
+	private String category;
+	private String text;
+	
+	public void setQuestionNo(){
+		this.questionNo=;
+	}
+	public void setCategory(){
+		this.category=;
+	}
+	public void setText(){
+		this.text=;
+	}
+	public int getQuestionNo(){
+		return ;
+	}
+	public String getCategory(){
+		return ;
+	}
+	public String getText(){
+		return ;
+	}
+	public void addQuestion(CscExample qu){
+	
+	}
+	public void removeQuestion(int quId){
+	
+	}
+	public void editQuestion(int quID){
+	
+	}
+}
+
